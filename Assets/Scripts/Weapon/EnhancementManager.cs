23d68ce02c977ce80e6c5d3abdc32f2a2d676c34
김대열 <ajduf7180@gmail.com>
2023-10-14 21:30:44 +0900

    [Update] 강화 선택 플레이어당 한 번씩 하도록 적용

--- Assets/Scripts/Weapon/EnhancementManager.cs
+++ Assets/Scripts/Weapon/EnhancementManager.cs
@@ -1,28 +1,41 @@
 using System;
 using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 using Weapon.Data;
+using Weapon.Model;
 using Random = UnityEngine.Random;
 
 namespace Weapon
 {
-    public class EnhancementManager : MonoBehaviour
+    public class EnhancementManager : MonoBehaviourPunCallbacks
     {
         //todo migrate to data manager
         private const string ENHANCEMENT_CSV_FILE = "enhancement_dataset";
         private Camera _camera;
 
+        [field: SerializeField] public Color[] PlayerColors { get; private set; }
         [SerializeField] private Canvas cards;
         [SerializeField] private EnhanceCardUI cardPrefab;
 
         private List<EnhancementDataEntry> _dataEntries;
-        private int _headcount = 5;
+
+        //todo get from gameManager
+        private int _headcount = 2;
+        private int CardCount => _headcount + 2;
+
+        private HashSet<int> _isPlayerSelectCard;
+        private EnhanceCardUI[] _cards;
+        public event Action<int, EnhancementData> OnEnhancementEvent;
+
 
         private void Awake()
         {
-            //todo get headcount from gameManager??
             LoadDataSet();
             _camera = Camera.main;
+            _isPlayerSelectCard = new HashSet<int>();
+            _cards = new EnhanceCardUI[CardCount];
         }
 
         private void Start()
@@ -36,31 +49,34 @@ namespace Weapon
             _dataEntries = CsvReader.ReadCsvFromResources<EnhancementDataEntry>(ENHANCEMENT_CSV_FILE, 1);
         }
 
-        public void CreateCards()
+        //game manager 가 다음 라운드로 넘어간 경우 호출해야됨 그러나 씬을 넘기는 구조라면 private 으로 해도 됨
+        private void CreateCards()
         {
             int repeat = 0;
             int cnt = 0;
-            bool[] selected = new bool[_dataEntries.Count];
-            while (repeat < 10000 && cnt < _headcount + 2)
+            bool[] isCreated = new bool[_dataEntries.Count];
+            while (repeat < 10000 && cnt < CardCount)
             {
                 repeat++;
                 int idx = Random.Range(0, _dataEntries.Count);
-                if (selected[idx])
+                if (isCreated[idx])
                 {
                     continue;
                 }
 
                 cnt++;
-                selected[idx] = true;
+                isCreated[idx] = true;
             }
 
             cnt = 0;
             float width = 0f;
             for (int i = 0; i < _dataEntries.Count; i++)
             {
-                if (selected[i])
+                if (isCreated[i])
                 {
                     EnhanceCardUI card = Instantiate(cardPrefab, cards.transform, false);
+                    _cards[cnt] = card;
+
                     card.SetEnhancementData(_dataEntries[i].ToEnhancementData());
                     if (width == 0)
                     {
@@ -79,12 +95,24 @@ namespace Weapon
             Vector3 centerPosition = _camera.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
             Vector3 startPosition = centerPosition - (spacing) * ((_headcount + 2) / 2f);
             startPosition.x += spacing.x / 2f;
-            card.transform.position = startPosition + (spacing * index);
+            card.Arrange(this, startPosition + spacing * index, index);
         }
 
-        public void SelectCard(int playerIndex)
+        public void SelectCard(int playerIndex, int cardIndex, EnhancementData data)
         {
             //on click event callback 
+            Debug.Log($"select Card-P :{playerIndex} ,C : {cardIndex}, data:{data.Name} ");
+
+
+            //todo synchronize state
+            if (_isPlayerSelectCard.Contains(playerIndex))
+            {
+                return;
+            }
+
+            _isPlayerSelectCard.Add(playerIndex);
+            OnEnhancementEvent?.Invoke(playerIndex, data);
+            _cards[cardIndex].SelectEnhancement(PlayerColors[playerIndex - 1]);
         }
     }
 }
