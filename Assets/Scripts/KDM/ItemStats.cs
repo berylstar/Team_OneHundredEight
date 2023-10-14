d48713914237114cc2df5e5e4a799193858f8be9
KimDaeMins <68540137+KimDaeMins@users.noreply.github.com>
2023-10-15 01:54:38 +0900

    [Add]ItemStat.cs/ItemStatSO.cs/Define.cs/Item.cs
    [Add]UnityEngine/Asset/Scripts/KDM/Define.cs
    Define : Enum과 매크로? 들을 모아둘 생각입니다
    
    [Add]UnityEngine/Asset/Scripts/KDM/Item.cs
    Item : 실제로 플레이어에 적용되는 아이템 효과입니다.
    [Add]UnityEngine/Asset/Scripts/KDM/ItemStats.cs
    [Add]UnityEngine/Asset/Scripts/KDM/ItemStatSO.cs
    ItemStat : 아이템이 배열로 들고있을 아이템 스텟들입니다 확장성이 좀 있을듯합니다.

---
+++ Assets/Scripts/KDM/ItemStats.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal class ItemStats
+{
+    [field: SerializeField] public float Duration { get; set; }
+    [field: HideInInspector] public bool isTimed { get; private set; }
+    public ItemStatSO statSO;
+}
