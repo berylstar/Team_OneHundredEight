9db0f807c2816148cf67214f055fd3e239f48bc3
KimDaeMins <68540137+KimDaeMins@users.noreply.github.com>
2023-10-16 18:26:52 +0900

    [Add & Update] PlayerStat.cs 추가 PlayerStatHandler업데이트
    [Update]UnityEngine/Asset/Scripts/Minsang/PlayerStatHandler.cs 업데이트
    스파르타타운 방식으로 좀 수정봤습니다
    [Add]UnityEngine/Asset/Scripts/Minsang/PlayerStat.cs 업데이트
    스파르타타운 방식으로 수정보는 과정에서 필요해서 집어넣었습니다.

--- Assets/Scripts/Minsang/PlayerStatHandler.cs
+++ Assets/Scripts/Minsang/PlayerStatHandler.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System;
 
 using Photon.Pun;
 
@@ -8,34 +10,132 @@ public class PlayerStatHandler : MonoBehaviour, IPunObservable
 {
     [SerializeField] private PlayerStatSO initialStat;
 
-    [field: SerializeField] public int HP { get; private set; } // 인스펙터에서 확인용. 추후 제거
-    [field: SerializeField] public int MaxHp { get; private set; }
-    [field: SerializeField] public float MoveSpeed { get; private set; }
-    [field: SerializeField] public float JumpForce { get; private set; }
     [field: SerializeField] public WeaponData Weapon { get; private set; }
+
+    public PlayerStat CurrentStat { get; private set; }
+    public LinkedList<PlayerStat> statModifiers = new LinkedList<PlayerStat>();
+ 
+    [SerializeField] private float healthChangeDelay = .5f;
+    private float _timeSinceLastChange = float.MaxValue;
+    private bool _invincibility = false;
+
+    public event Action OnDamage;
+    public event Action OnHeal;
+    public event Action OnDeath;
+    public event Action OnInvincibilityEnd;
+
+    //TODO : 나중에 헬스시스템으로 따로 빼는게 괜찮긴할듯합니다
+    public void SetInvincible(bool onoff)
+    {
+        _invincibility = onoff;
+    }
+    public bool ChangeHealth(float change)
+    {
+        if (change == 0 || _timeSinceLastChange < healthChangeDelay || _invincibility)
+        {
+            return false;
+        }
+
+        _timeSinceLastChange = 0f;
+        CurrentStat.HP += change;
+        CurrentStat.HP = Mathf.Clamp(CurrentStat.HP, 0.0f, CurrentStat.MaxHp);
+
+        if (change > 0)
+        {
+            OnHeal?.Invoke();
+        }
+        else
+        {
+            OnDamage?.Invoke();
+        }
+
+        if (CurrentStat.HP <= 0.0f)
+        {
+            OnDeath?.Invoke();
+        }
+
+        return true;
+    }
+    public void SetHealth(float change)
+    {
+        //이렇게 써도되겠지만 고민좀 해봅시다 네..
+        CurrentStat.HP = change;
+    }
 
     private void Start()
     {
-        HP = initialStat.MaxHp;
-        MaxHp = initialStat.MaxHp;
-        MoveSpeed = initialStat.MoveSpeed;
-        JumpForce = initialStat.JumpForce;
         Weapon = GameManager.Instance.Weapons[initialStat.WeaponIndex];
 
+        InitPlayerStat();
+
         GameManager.Instance.PlayerStats.Add(this);
+    }
+    public void InitPlayerStat()
+    {
+        CurrentStat.HP = initialStat.MaxHp;
+        CurrentStat.MaxHp = initialStat.MaxHp;
+        CurrentStat.MoveSpeed = initialStat.MoveSpeed;
+        CurrentStat.JumpForce = initialStat.JumpForce;
+    }
+    public void AddStatModifier(PlayerStat statModifier)
+    {
+        statModifiers.AddLast(statModifier);
+        UpdateCharacterStats();
+    }
+    public void RemoveStatModifier(PlayerStat statModifier)
+    {
+        statModifiers.Remove(statModifier);
+        UpdateCharacterStats();
+    }
+    public void UpdateCharacterStats()
+    {
+        SetBaseStat();
+
+        foreach (PlayerStat modifier in statModifiers.OrderBy(x => x.statsChangeType))
+        {
+            if (modifier.statsChangeType == Define.StatsChangeType.Override)
+            {
+                UpdateStat((o, o1) => o1, modifier);
+            }
+            else if (modifier.statsChangeType == Define.StatsChangeType.Add)
+            {
+                UpdateStat((o, o1) => o + o1, modifier);
+            }
+            else if (modifier.statsChangeType == Define.StatsChangeType.Multiple)
+            {
+                UpdateStat((o, o1) => o * o1, modifier);
+            }
+        }
+    }
+
+    private void SetBaseStat()
+    {
+        CurrentStat.MaxHp = initialStat.MaxHp;
+        CurrentStat.MoveSpeed = initialStat.MoveSpeed;
+        CurrentStat.JumpForce = initialStat.JumpForce;
+    }
+    private void UpdateStat(Func<float, float, float> operation, PlayerStat newModifier)
+    {
+        CurrentStat.MaxHp = (int)operation(CurrentStat.MaxHp, newModifier.MaxHp);
+        CurrentStat.JumpForce = operation(CurrentStat.JumpForce, newModifier.JumpForce);
+        CurrentStat.MoveSpeed = operation(CurrentStat.MoveSpeed, newModifier.MoveSpeed);
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting)
         {
-            stream.SendNext(HP);
-            stream.SendNext(MaxHp);
+            stream.SendNext(CurrentStat.HP);
+            stream.SendNext(CurrentStat.MaxHp);
+            stream.SendNext(CurrentStat.JumpForce);
+            stream.SendNext(CurrentStat.MoveSpeed);
         }
         else
         {
-            HP = (int)stream.ReceiveNext();
-            MaxHp = (int)stream.ReceiveNext();
+            CurrentStat.HP = (float)stream.ReceiveNext();
+            CurrentStat.MaxHp = (float)stream.ReceiveNext();
+            CurrentStat.JumpForce = (float)stream.ReceiveNext();
+            CurrentStat.MoveSpeed = (float)stream.ReceiveNext();
         }
     }
 }
