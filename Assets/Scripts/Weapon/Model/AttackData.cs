29e3146bed4641a0cd53e60bf676148c8efd1a16
minsang <minsang0610@naver.com>
2023-10-17 09:57:08 +0900

    [UPDATE] PlayerAttackHandler

--- Assets/Scripts/Weapon/Model/AttackData.cs
+++ Assets/Scripts/Weapon/Model/AttackData.cs
@@ -4,21 +4,34 @@ using Unity.VisualScripting;
 [Serializable]
 public class AttackData
 {
+    public Define.StatsChangeType statsChangeType;
     public int bulletDamage;
     public float bulletSpeed;
     public int maxMagazine;
     public float shotInterval;
     public float reloadTime;
 
-    public static AttackData operator +(AttackData a, AttackData b)
-    {
-        return new AttackData()
-        {
-            bulletDamage = a.bulletDamage + b.bulletDamage,
-            bulletSpeed = a.bulletSpeed + b.bulletSpeed,
-            maxMagazine = a.maxMagazine + b.maxMagazine,
-            shotInterval = a.shotInterval + b.shotInterval,
-            reloadTime = a.reloadTime + b.reloadTime
-        };
-    }
+    //public static AttackData operator +(AttackData a, AttackData b)
+    //{
+    //    return new AttackData()
+    //    {
+    //        bulletDamage = a.bulletDamage + b.bulletDamage,
+    //        bulletSpeed = a.bulletSpeed + b.bulletSpeed,
+    //        maxMagazine = a.maxMagazine + b.maxMagazine,
+    //        shotInterval = a.shotInterval + b.shotInterval,
+    //        reloadTime = a.reloadTime + b.reloadTime
+    //    };
+    //}
+
+    //public static AttackData operator *(AttackData a, AttackData b)
+    //{
+    //    return new AttackData()
+    //    {
+    //        bulletDamage = a.bulletDamage * b.bulletDamage,
+    //        bulletSpeed = a.bulletSpeed * b.bulletSpeed,
+    //        maxMagazine = a.maxMagazine * b.maxMagazine,
+    //        shotInterval = a.shotInterval * b.shotInterval,
+    //        reloadTime = a.reloadTime * b.reloadTime
+    //    };
+    //}
 }
