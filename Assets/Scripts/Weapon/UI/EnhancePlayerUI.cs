c1dca3f4297ac0c7534e75a7b1e934fc51cb17aa
김대열 <ajduf7180@gmail.com>
2023-10-18 14:29:25 +0900

    [Update] 무기 증강 효과 순서 ui 적용

--- Assets/Scripts/Weapon/UI/EnhancePlayerUI.cs
+++ Assets/Scripts/Weapon/UI/EnhancePlayerUI.cs
@@ -1,21 +1,42 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
+using Text = TMPro.TextMeshProUGUI;
 
 namespace Weapon.UI
 {
     public class EnhancePlayerUI : MonoBehaviour
     {
-        [field: SerializeField] public Image PlayerImage { get; private set; }
-        [field: SerializeField] public Image CheckImage { get; private set; }
-
-        public void SetPlayerImage(Sprite image)
+        public record UiState
         {
-            PlayerImage.sprite = image;
+            public string Nickname;
+            public Color Color;
+            public Sprite Image;
+            public bool IsPlayerTurn;
+            public bool IsSelected;
         }
 
-        public void Check()
+        [SerializeField] private Image playerImage;
+        [SerializeField] private Image backgroundImage;
+        [SerializeField] private Image checkImage;
+        [SerializeField] private Image orderArrowImage;
+        [SerializeField] private Text nicknameText;
+
+        private UiState _state;
+        public UiState CurrentState => _state;
+
+        public void ChangeState(UiState newState)
         {
-            CheckImage.gameObject.SetActive(true);
+            _state = newState;
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            nicknameText.text = CurrentState.Nickname;
+            backgroundImage.color = CurrentState.Color;
+            checkImage.gameObject.SetActive(CurrentState.IsSelected);
+            orderArrowImage.gameObject.SetActive(CurrentState.IsPlayerTurn);
         }
     }
 }
