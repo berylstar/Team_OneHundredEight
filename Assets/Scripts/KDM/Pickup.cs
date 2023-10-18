d54588f6dd26d5837b99b821958d32614d289f44
KimDaeMins <68540137+KimDaeMins@users.noreply.github.com>
2023-10-18 17:02:33 +0900

    [Fix] 아이템먹으면 죽는거 수정
    곱셈연산일때 문제였는데.. 좀 고민해보겠슴다

--- Assets/Scripts/KDM/Pickup.cs
+++ Assets/Scripts/KDM/Pickup.cs
@@ -68,8 +68,8 @@ public class Pickup : MonoBehaviourPunCallbacks, IPunObservable
         //느린쪽에 맞춘 히트판정 , 포톤을 쓰는 녀석들만 히트판정.(안쓰는게있나?)
          if(!col.TryGetComponent<PhotonView>(out PhotonView pv))
             return;
-        if (_PV.IsMine || !pv.IsMine)
-            return;
+        //if (_PV.IsMine || !pv.IsMine)
+        //    return;
 
         if (0 != (_playerCollisionLayer.value & (1 << col.gameObject.layer)))
         {
